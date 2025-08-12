317d7679d5136d66f2cb850c70887e96e5b1f8b0
ndurushima <ndu1994@gmail.com>
2025-08-12 15:44:51 -0500

    did the app.route for prodcuts using GET method and filtered

--- app.py
+++ app.py
@@ -16,9 +16,13 @@ def home():
 
 # TODO: Implement GET /products route that returns all products or filters by category
 
-@app.route("/products")
+@app.route("/products", methods=["GET"])
 def get_products():
-    pass  # TODO: Return all products or filter by ?category=
+    category = request.args.get("category")
+    if category:
+        filtered = [item for item in products if item["category"] == category]
+        return jsonify(filtered), 200
+    return jsonify(products), 200
 
 # TODO: Implement GET /products/<id> route that returns a specific product by ID or 404
 
